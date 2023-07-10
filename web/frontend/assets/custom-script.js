f51b686c3b466a12bd1ece432c4d11a07daa41ea
Your Name <your-email@example.com>
2023-07-10 17:53:06 +0530

    Remove all files and folders from repository

--- web/frontend/assets/custom-script.js
+++
@@ -1,8 +0,0 @@
-const button = document.createElement("button");
-button.innerHTML = "Share on WhatsApp";
-button.onclick = function() {
-  // Code to share cart on WhatsApp goes here
-};
-
-const cartNode = document.querySelector(".cart");
-cartNode.appendChild(button);
--- web/frontend/assets/index.js
+++
@@ -1,3 +0,0 @@
-export { default as notFoundImage } from "./empty-state.svg";
-export { default as trophyImage } from "./home-trophy.png";
-export { default as wpcartImage } from "./whatsapp-sharing-cart.jpg";
